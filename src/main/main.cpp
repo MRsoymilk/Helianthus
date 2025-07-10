bc0d2509d1d9d6432413cd7c889a948ce0a0ab10
MRsoymilk <codermrsoymilk@gmail.com>
2025-07-10 16:29:00 +0800

    增加icon，优化窗口命名

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/res/icons/Helianthus.png"));
     MainWindow w;
     w.show();
     return a.exec();
