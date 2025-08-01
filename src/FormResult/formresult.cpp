99b14f11544f429bd33c8f1db1e638f25fa142d5
MRsoymilk <codermrsoymilk@gmail.com>
2025-08-01 11:02:45 +0800

    update page

--- src/FormResult/formresult.cpp
+++ src/FormResult/formresult.cpp
@@ -21,7 +21,7 @@ void FormResult::init()
         el.addEventListener('click', e => e.preventDefault());
     });
 )");
-    ui->widget->load(QUrl("qrc:/res/page/空.html"));
+    ui->widget->load(QUrl("qrc:/res/page/Empty.html"));
 }
 
 void FormResult::showResult(RESULT result)
@@ -32,10 +32,10 @@ void FormResult::showResult(RESULT result)
     lastResult = result;
     switch (result) {
     case RESULT::Water:
-        ui->widget->load(QUrl("qrc:/res/page/水.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Water.html"));
         break;
     case RESULT::Alcohol75:
-        ui->widget->load(QUrl("qrc:/res/page/75酒精.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Alcoholic_beverage.html"));
         break;
     case RESULT::C2H4O2:
         ui->widget->load(QUrl("qrc:/res/page/C2H4O2.html"));
@@ -44,37 +44,37 @@ void FormResult::showResult(RESULT result)
         ui->widget->load(QUrl("qrc:/res/page/C2H6O.html"));
         break;
     case RESULT::CornOil:
-        ui->widget->load(QUrl("qrc:/res/page/玉米油.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Corn_oil.html"));
         break;
     case RESULT::EmptyBottle:
-        ui->widget->load(QUrl("qrc:/res/page/空瓶.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Bottle.html"));
         break;
     case RESULT::Empty:
-        ui->widget->load(QUrl("qrc:/res/page/空.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Empty.html"));
         break;
     case RESULT::Starch:
-        ui->widget->load(QUrl("qrc:/res/page/淀粉.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Starch.html"));
         break;
     case RESULT::Sugar:
-        ui->widget->load(QUrl("qrc:/res/page/糖.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Sugar.html"));
         break;
     case RESULT::Salt:
-        ui->widget->load(QUrl("qrc:/res/page/食盐.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Salt.html"));
         break;
     case RESULT::NaHCO3:
-        ui->widget->load(QUrl("qrc:/res/page/碳酸氢钠.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Sodium_bicarbonate.html"));
         break;
     case RESULT::TongSui:
-        ui->widget->load(QUrl("qrc:/res/page/糖水.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Tong_sui.html"));
         break;
     case RESULT::OliveOil:
-        ui->widget->load(QUrl("qrc:/res/page/橄榄油.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Olive_oil.html"));
         break;
     case RESULT::SesameOil:
-        ui->widget->load(QUrl("qrc:/res/page/芝麻油.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Sesame_oil.html"));
         break;
     case RESULT::SunflowerOil:
-        ui->widget->load(QUrl("qrc:/res/page/葵花籽油.html"));
+        ui->widget->load(QUrl("qrc:/res/page/Sunflower_oil.html"));
         break;
     }
 }
