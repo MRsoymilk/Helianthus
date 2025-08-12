2d8d70cc69a302c07e0487c3af1c8d1cb961d224
MRsoymilk <codermrsoymilk@gmail.com>
2025-08-12 12:05:58 +0800

    single application

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,10 +1,22 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QMessageBox>
+#include <QSharedMemory>
+#include "g_define.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSharedMemory sharedMemory;
+    sharedMemory.setKey("Helianthus_soymilk");
+
+    if (!sharedMemory.create(1)) {
+        QMessageBox::warning(nullptr, TITLE_WARNING, QObject::tr("Application is already running!"));
+
+        return 0;
+    }
+
     a.setWindowIcon(QIcon(":/res/icons/Helianthus.png"));
     MainWindow w;
     w.show();
